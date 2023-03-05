eeefefe62244a75f2031f4d778c2948be752fa21
alekseygnsu <a.polevik@g.nsu.ru>
2023-03-05 19:39:30 +0700

    gei2

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import turtle as t
 
 t = turtle.Turtle()
 t.speed(1)
+t.pencolor("black")
+
 def Triangle1(x,y,size,angle):
   t.up()
-  t.color("orange")
-
+  t.fillcolor("orange")
   t.goto(x,y)
   t.right(angle)
   t.down()
-  t.right(angle)
+  t.begin_fill()
   t.forward(100*size)
   t.left(110)
   t.forward(146*size)
   t.left(140)
   t.forward(146*size)
+  t.end_fill()
 
 
   """
@@ -24,20 +26,38 @@ def Triangle1(x,y,size,angle):
   """
   pass
 
-# Triangle1(0,0,2,0)
+#Triangle1(0,0,2,0)
+
+def Triangle2(x,y,size):
+  t.up()
+  t.fillcolor("orange")
+  t.goto(x,y)
+  t.left(90)
+  t.down()
+  t.begin_fill()
+  t.forward(100*size)
+  t.right(100)
+  t.forward(10*size)
+  t.right(85)
+  t.forward(100*size)
+  t.end_fill()
+  pass
+Triangle2(0,0,2)
 
 def Circle(x,y, size, color):
   t.up()
   t.goto(x, y)
-  t.color(color)
+  t.fillcolor(color)
   t.down()
-
+  t.begin_fill()
   t.circle(size)
+  t.end_fill()
 
   pass
 
 
-Circle(0,0,100,"red")
+# Circle(0,0,100,"red")
+
 
 
 
