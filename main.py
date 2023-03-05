8b59ee3fa6b34a0b26918497d4a526650d146cd9
Elizar54 <y.elizarev@g.nsu.ru>
2023-03-05 18:00:39 +0700

    hello1

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-import turtle
-def Circle():
-  """
-  Function draws circle.
-  :return:
-  """
-  pass
+import turtle as t
 
-def Triangle():
-  '''function returns rtiangle/
-  : '''
-  pass
 
-#hello
