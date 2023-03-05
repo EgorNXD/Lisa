50035889594b97de21bafc9cc168c21380f0f748
EgorNXD <e.nechaev@g.nsu.ru>
2023-03-05 21:44:30 +0700

    -добавил функцию прямоугольника

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def Triangle(x, y, size, angle):
   t.forward(146.1902*size)
   t.left(140)
   t.forward(146.1902*size)
-  t.seth(0)
   t.end_fill()
+  t.seth(0)
 
   """
   Draws triangle.
@@ -48,8 +48,8 @@ def Tail(x, y, size):
   t.forward(10*size)
   t.right(100)
   t.forward(28.7939*size)
-  t.seth(0)
   t.end_fill()
+  t.seth(0)
 
   """
   Draws triangle-tail.
@@ -67,13 +67,13 @@ def Circle(x, y, size, color):
   t.down()
   t.begin_fill()
   t.circle(size)
-  t.seth(0)
   t.end_fill()
+  t.seth(0)
 
   """
   Draws circle.
   x,y - start coordinates.
-  size - size of the circle.
+  size - size of the radius of the circle.
   color - color of filling.
   """
   pass
@@ -91,10 +91,42 @@ def Smile(x, y, size, width):
   t.pencolor("black")
   t.seth(0)
 
+  """
+  Draws half circle.
+  x,y - start coordinates.
+  size - size of radius of the half circle.
+  color - color of filling.
+  """
+  pass
+
+#Rectangle
+def Rec(x, y, a, b, color):
+  t.up()
+  t.goto(x, y)
+  t.fillcolor(color)
+  t.down()
+  t.begin_fill()
+  for i in range(2):
+    t.forward(a)
+    t.left(90)
+    t.forward(b)
+    t.left(90)
+  t.end_fill()
+  t.seth(0)
+  """
+  Draws rectangle.
+  x,y - start coordinates.
+  a,b - sides length.
+  color - color of filling.
+  """
+  pass
+
+
+
 #Test zone
 #Triangle(0,0,1,0)
 #Tail(100,0,3)
 #Smile(0,0,100,5)
 #Circle(0,50,50,"red")
-
+#Rec(0,0,100,50,"yellow")
 turtle.done()
