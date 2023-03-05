6ce5d6569a3759ab196439c2238436f9f97a9554
alekseygnsu <a.polevik@g.nsu.ru>
2023-03-05 19:14:08 +0700

    gei

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 import turtle
 import turtle as t
 
-pen = turtle.Turtle()
-def Triangle1(x,y,sizee,angle):
-  turtle.goto(x,y)
-  pen.right(angle)
+
+t = turtle.Turtle()
+t.speed(1)
+def Triangle1(x,y,size,angle):
+  t.up()
+  t.color("orange")
+
+  t.goto(x,y)
+  t.right(angle)
+  t.down()
+  t.right(angle)
+  t.forward(100*size)
+  t.left(110)
+  t.forward(146*size)
+  t.left(140)
+  t.forward(146*size)
+
 
   """
   Function draws Triangle1.
   """
   pass
+
+# Triangle1(0,0,2,0)
+
+def Circle(x,y, size, color):
+  t.up()
+  t.goto(x, y)
+  t.color(color)
+  t.down()
+
+  t.circle(size)
+
+  pass
+
+
+Circle(0,0,100,"red")
+
+
+
 
