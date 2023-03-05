058ddfc09e407db08b4cbe27f145a3df5df38c62
EgorNXD <e.nechaev@g.nsu.ru>
2023-03-05 18:33:28 +0700

    triangle 1

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import turtle
 import turtle as t
 
-tut = turtle.Turtle()
-def Triangle1(x,y,size):
+pen = turtle.Turtle()
+def Triangle1(x,y,sizee,angle):
+  turtle.goto(x,y)
+  pen.right(angle)
 
   """
   Function draws Triangle1.
